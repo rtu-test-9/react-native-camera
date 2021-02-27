cc62ad226429ef8dc7523bebe4b5a2703e58841f
obisessionprime@gmail.com <obisessionprime@gmail.com>
2021-02-27 17:09:58 +0300

    New repo commit 3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 //This is branch1 commit3
 //This is branch1 commit1 from new repo
 //This is branch1 commit2 from new repo
+//This is branch1 commit3 from new repo
 import RNCamera, { type Status as _CameraStatus } from './RNCamera';
 import FaceDetector from './FaceDetector';
 
